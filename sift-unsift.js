683da0386a8caa83bf1bfa98844cf9fb0612fb29
asad <asaduzzamanprogrammer@gmail.com>
2024-02-04 16:04:24 +0600

    sift-unsift

---
+++ sift-unsift.js
@@ -0,0 +1,16 @@
+// const numbers = [12, 13, 14, 15, 16, 17, 18, 19];
+// console.log(numbers);
+
+// numbers.unshift(11)
+// console.log(numbers);
+
+// numbers.shift()
+// console.log(numbers);
+
+const friends = ["salma", "rumana", "taskin", "snigdha"];
+
+friends.unshift("nabila");
+
+friends.shift();
+
+console.log(friends)
