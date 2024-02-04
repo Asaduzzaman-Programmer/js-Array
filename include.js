53a789c9852f1eb92dfaed20a18ef979b4b1483f
asad <asaduzzamanprogrammer@gmail.com>
2024-02-04 16:33:40 +0600

    17no module complete

---
+++ include.js
@@ -0,0 +1,13 @@
+const friends = ["salma", "rumana", "taskin", "snigdha"];
+console.log(friends);
+console.log(friends.includes("rumana"))
+console.log(friends.includes("taskin"))
+
+if(friends.includes("taskin")){
+    console.log("aami tar sathe vinno jogot jabo")
+}
+else{
+    console.log("jiboneo vinno jogot jabo na")
+}
+console.log(friends.indexOf("snigdha"));
+console.log(friends)
