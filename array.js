520d5c5cbc67eb7a551115ea5a0987be90302c74
asad <asaduzzamanprogrammer@gmail.com>
2024-02-04 14:55:36 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,10 @@
+// numbers in array 
+const numbers = [52, 63, 54, 85];
+console.log(numbers);
+// boolean in array
+const booleans = [true, false, true, false];
+console.log(booleans);
+// strings in array
+const strings = ['abul', 'kabul', 'mokbul', 'safin']
+console.log(strings);
+
