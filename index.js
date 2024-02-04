aa6b7e81244a447821e52b9c87f24a30093cb73f
asad <asaduzzamanprogrammer@gmail.com>
2024-02-04 15:31:18 +0600

    index

---
+++ index.js
@@ -0,0 +1,10 @@
+const numbers = [10, 52, 89, 78, 96];
+let abc =0;
+abc = 45;
+numbers[0] = abc
+console.log(numbers);
+// console.log(numbers[0]);
+
+// const fourth = numbers[3];
+// console.log(fourth);
+
---
+++ lenght.js
@@ -0,0 +1,3 @@
+const numbers = [ 1, 2, 3, 4, 5, 5, 5, 4, 7, 1, 2, 3, 4, 5, 5, 5, 4, 7];
+console.log(numbers);
+console.log(numbers.length);
